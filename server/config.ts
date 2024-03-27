e71da622f767f45b75490da3eda46336e8b6bfb5
krisneiras <kristygns21@gmail.com>
2024-03-27 11:19:20 +0100

    connect app to database

---
+++ server/config.ts
@@ -0,0 +1,6 @@
+import 'dotenv/config';
+
+export const DB_NAME = <string> process.env.DB_NAME;
+export const DB_USER= <string> process.env.DB_USER;
+export const DB_PASSWORD= <string> process.env.DB_PASSWORD;
+export const DB_PORT = <string> process.env.DB_PORT;
