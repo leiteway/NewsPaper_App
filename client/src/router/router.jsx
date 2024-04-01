4f26573b874a3827bfa33f778e0c5f32e81e93dc
Fátima Cosme Salvador <146832705+pointfs@users.noreply.github.com>
2024-04-01 09:25:23 +0200

    folders_front

---
+++ client/src/router/router.jsx
@@ -0,0 +1,46 @@
+import { createBrowserRouter } from "react-router-dom";
+import LayoutPublic from "../components/layout";
+import Home from "../components/home/Home";
+import SignInForm from "../components/signInForm/SignInForm";
+import RegisterForm from "../components/registerForm/RegisterForm";
+import NewPost from "../components/newPost/NewPost";
+import EditPost from "../components/editPost/EditPost";
+import Card from "../components/card/Card";
+
+
+export const router = createBrowserRouter([
+  {
+      path: "/",
+      element: <LayoutPublic />,
+      children: [
+    
+    {
+        path: "/SignIn",
+        element: <SignInForm/>,
+    }
+    {
+        path: "/Register",
+        element: <RegisterForm/>,
+    }
+    {
+        path: "/",
+        element: <Home/>,
+    },
+    {
+      path: "/NewPost",
+      element: <NewPost/>,
+    },
+    {
+      path: "/EditPost/:id",
+      element: <EditPost/>
+    },
+   
+    {
+      path: "/card/:id",
+      element: <Card/>
+    }
+  ],
+  }
+  ]);
+
+export default router;
