2b416b56ffe9446ad102e71a2b0d802adc414e5a
Leite <leite.way@gmail.com>
2024-04-01 11:39:39 +0200

    create-folders-for-pages-and-services-and-modified-main-and-router-file

---
+++ client/src/router/router.tsx
@@ -0,0 +1,47 @@
+import { createBrowserRouter } from "react-router-dom";
+import SignInSide from "../components/registerForm/SingIn";
+// import LayoutPublic from "../components/layout/";
+// import Home from "../components/home/Home";
+
+// import RegisterForm from "../components/registerForm/RegisterForm";
+// import NewPost from "../components/newPost/NewPost";
+// import EditPost from "../components/editPost/EditPost";
+// import Card from "../components/card/Card";
+
+
+export const router = createBrowserRouter([
+  {
+      path: "/",
+      // element: <LayoutPublic />,
+      children: [
+    
+    {
+        path: "/SignIn",
+        element: <SignInSide/>,
+    },
+    {
+        path: "/Register",
+        // element: <RegisterForm/>,
+    },
+    {
+        path: "/",
+        // element: <Home/>,
+    },
+    {
+      path: "/NewPost",
+      // element: <NewPost/>,
+    },
+    {
+      path: "/EditPost/:id",
+      // element: <EditPost/>
+    },
+   
+    {
+      path: "/card/:id",
+      // element: <Card/>
+    }
+  ],
+  }
+  ]);
+
+export default router;
