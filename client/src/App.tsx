685d4a85210a60d8523b4228076c03d65bebfea1
Leite <leite.way@gmail.com>
2024-04-01 10:57:17 +0200

    creating-signin-component-on-feature-component-cards

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import './App.css'
 import SignInSide from './components/registerForm/SingIn'
 
 function App() {
--- client/src/components/registerForm/SingIn.tsx
+++ client/src/components/registerForm/SingIn.tsx
@@ -49,7 +49,7 @@ export default function SignInSide() {
           sm={4}
           md={7}
           sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+            backgroundImage: 'url(src/assets/images/5052197.jpg)',
             backgroundRepeat: 'no-repeat',
             backgroundColor: (t) =>
               t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
