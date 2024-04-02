4167573dfa3ead99e44b3a7a1261fc33a95b63ca
albamartinmz <albamartinsonido@gmail.com>
2024-04-02 11:08:58 +0200

    try createUser function

--- server/controllers/UserController.ts
+++ server/controllers/UserController.ts
@@ -1,7 +1,8 @@
+import bcrypt from 'bcryptjs';
 import UserModel from "../models/UserModel";
 import { Response,Request } from 'express';
 
-//GET
+//GET ONE
 export const getOneUser = async (req: Request, res:Response) =>{
 
     try{
@@ -13,3 +14,23 @@ export const getOneUser = async (req: Request, res:Response) =>{
         res.status(500).json({message: error.message})
     }
 }
+
+//POST
+export const createUser = async (req: Request, res: Response) => {
+    try {
+        const { name, email, password } = req.body;
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        
+        const newUser = await UserModel.create({
+            name,
+            email,
+            password: hashedPassword
+        });
+
+        res.status(201).json(newUser);
+        
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
