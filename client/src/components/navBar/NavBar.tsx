35566a47a6771ac3cbd951379d92a4f8749fb9f9
DesireeCSilva <desiresilva1987@gmail.com>
2024-04-02 11:23:30 +0200

    updates-on-navBar

--- client/src/components/navBar/NavBar.tsx
+++ client/src/components/navBar/NavBar.tsx
@@ -1,16 +1,31 @@
 import './NavBar.css'
+// import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+export default function NavBar() {
+  return (
+    <div className="navbar">
+        <img className="logotipo" src='../src/assets/public/logotipo.png' alt="" />
+        <Box className="searchBar" >
+            <TextField className="textField" label="Busca tu noticia" variant='outlined' id="fullWidth" />
+        </Box>
+        <img src='../src/assets/public/avatar-admin-screen.png' alt="" />
+    </div>
+  );
+}
 // import { Link } from 'react-router-dom'
 
-const NavBar = () => {
-    return (
-    <>
-    <nav>
+// const NavBar = () => {
+//     return (
+//     <>
+//     <nav>
 
 
 
-    </nav>
-    </>
-    )
-}
+//     </nav>
+//     </>
+//     )
+// }
 
-export default NavBar
+// export default NavBar
