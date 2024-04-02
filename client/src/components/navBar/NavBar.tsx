f5042fba63feaed387cc1407b3d22b32ab79368a
DesireeCSilva <desiresilva1987@gmail.com>
2024-04-02 12:28:08 +0200

    navBar-finished

--- client/src/components/navBar/NavBar.tsx
+++ client/src/components/navBar/NavBar.tsx
@@ -1,31 +1,21 @@
 import './NavBar.css'
 // import * as React from 'react';
 import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import SearchIcon from '@mui/icons-material/Search';
 
 export default function NavBar() {
   return (
     <div className="navbar">
         <img className="logotipo" src='../src/assets/public/logotipo.png' alt="" />
-        <Box className="searchBar" >
-            <TextField className="textField" label="Busca tu noticia" variant='outlined' id="fullWidth" />
+        <Box>
+            <SearchIcon className='searchIcon'/>
+            <input className="searchBar" placeholder="Escribe tu búsqueda aquí..." />
         </Box>
+        <div className='userContainer'>
         <img src='../src/assets/public/avatar-admin-screen.png' alt="" />
+        <p>Fátima</p>
+        </div>
     </div>
   );
 }
-// import { Link } from 'react-router-dom'
 
-// const NavBar = () => {
-//     return (
-//     <>
-//     <nav>
-
-
-
-//     </nav>
-//     </>
-//     )
-// }
-
-// export default NavBar
