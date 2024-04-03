203137996ecad6c7f8cb1141075d772c2d7e3b14
DesireeCSilva <desiresilva1987@gmail.com>
2024-04-03 15:17:33 +0200

    progress-on-contact-sidebar

--- client/src/components/sideBar/SideBar.tsx
+++ client/src/components/sideBar/SideBar.tsx
@@ -7,33 +7,34 @@ import RocketLaunchIcon from '@mui/icons-material/RocketLaunch';
 
 export default function SideBar(){
   return (
-    <div>
-        <div className='sideBar-container'>
+    <>
+        <div className='sidebar-container'>
             <p className='title-sidebar'>My Feed</p>
             {<hr  />} 
             <section>
-           <p className='text-sidebar'><CottageIcon className='cottage-icon'/>Inicio</p>
-            <p className='text-sidebar'><EqualizerIcon className='equalizer-icon'/> Posts</p>
+           <p className='text-sidebar'><CottageIcon className='icon-sidebar'/>Inicio</p>
+            <p className='text-sidebar'><EqualizerIcon className='icon-sidebar'/> Posts</p>           
+            <p className='text-sidebar'><PushPinIcon className='icon-sidebar'/>Nuevo Post</p>
             </section>
-            <p className='text-sidebar'><PushPinIcon className='pin-icon'/>Nuevo Post</p>
+            <section>
             <p className='text-sidebar'>CUENTA</p>
-            <p className='text-sidebar'><PersonIcon className='person-icon'/>Perfil</p>
-            <p className='text-sidebar'><RocketLaunchIcon className='rocket-icon'/>Sign Out</p>
-            
-            
-            
+            <p className='text-sidebar'><PersonIcon className='icon-sidebar'/>Perfil</p>
+            <p className='text-sidebar'><RocketLaunchIcon className='icon-sidebar'/>Sign Out</p>
+            </section>
+            <section>  
             <p className='text-sidebar'>REDES SOCIALES</p>
-            <img className='icons-sidebar' src="src/assets/public/social icons.png" alt="Iconos de redes sociales" />   
-        
-            <div className='contact-container'>
-                <img src="src/assets/images/bg-sidebar.png" alt="" /> {/* Este es el bg del Need Help y tiene que estar de fondo */}
-                <img src="src/assets/public/Icon-contact-us.png" alt="Icono de contacto" />
-                <p className='text-sidebar'>¿Necesitas ayuda?</p>
-                <p className='text-sidebar'>Contacta con nosotras</p>
-                <button className='text-sidebar'>CONTACTAR</button>
-            </div> 
+            <img className='social-icons-sidebar' src="src/assets/public/social icons.png" alt="Iconos de redes sociales" />   
+            </section>
+
+            <section className='contact-container'>
+                <img className='contact-bg' src="src/assets/images/bg-sidebar.png" alt="" /> {/* Este es el background del Need Help y tiene que estar de fondo */}
+                <img className='icon-contact-sidebar' src="src/assets/public/Icon-contact-us.png" alt="Icono de contacto" />
+                <p className='text-contact-sidebar'>¿Necesitas ayuda?</p>
+                <p className='text-contact-sidebar'>Contacta con nosotras</p>
+                <button className='button-contact-sidebar'>CONTACTAR</button>
+            </section> 
         </div>   
-    </div>
+    </>
   )
 }
 
