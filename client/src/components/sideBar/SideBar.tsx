c6e21394c1b845afbe33848c5ab69cc934356fa2
DesireeCSilva <desiresilva1987@gmail.com>
2024-04-03 11:48:00 +0200

    progress-on-sidebar

--- client/src/components/sideBar/SideBar.tsx
+++ client/src/components/sideBar/SideBar.tsx
@@ -1,4 +1,9 @@
 import './SideBar.css'
+import CottageIcon from '@mui/icons-material/Cottage';
+import EqualizerIcon from '@mui/icons-material/Equalizer';
+import PushPinIcon from '@mui/icons-material/PushPin';
+import PersonIcon from '@mui/icons-material/Person';
+import RocketLaunchIcon from '@mui/icons-material/RocketLaunch';
 
 export default function SideBar(){
   return (
@@ -6,14 +11,19 @@ export default function SideBar(){
         <div className='sideBar-container'>
             <p className='title-sidebar'>My Feed</p>
             {<hr  />} 
-            <p className='text-sidebar'>Inicio</p>
-            <p className='text-sidebar'>Mis Posts</p>
+            <section>
+           <p className='text-sidebar'><CottageIcon className='cottage-icon'/>Inicio</p>
+            <p className='text-sidebar'><EqualizerIcon className='equalizer-icon'/> Posts</p>
+            </section>
+            <p className='text-sidebar'><PushPinIcon className='pin-icon'/>Nuevo Post</p>
             <p className='text-sidebar'>CUENTA</p>
-            <p className='text-sidebar'>Perfil</p>
-            <p className='text-sidebar'>Sign Out</p>
-            <p className='text-sidebar'>Nuevo Post</p>
+            <p className='text-sidebar'><PersonIcon className='person-icon'/>Perfil</p>
+            <p className='text-sidebar'><RocketLaunchIcon className='rocket-icon'/>Sign Out</p>
+            
+            
+            
             <p className='text-sidebar'>REDES SOCIALES</p>
-            <img src="src/assets/public/social icons.png" alt="Iconos de redes sociales" />   
+            <img className='icons-sidebar' src="src/assets/public/social icons.png" alt="Iconos de redes sociales" />   
         
             <div className='contact-container'>
                 <img src="src/assets/images/bg-sidebar.png" alt="" /> {/* Este es el bg del Need Help y tiene que estar de fondo */}
