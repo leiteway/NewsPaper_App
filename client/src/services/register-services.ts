b5597a83ad329a713a15968414d18f2d48429a2b
Leite <leite.way@gmail.com>
2024-04-05 11:00:29 +0200

    change-register-services-test-token-front

--- client/src/services/register-services.ts
+++ client/src/services/register-services.ts
@@ -1,16 +1,19 @@
 import axios from 'axios';
 
 interface RegisterData {
-    username: string;
-    password: string;
-    email: string;
+  username: string;
+  password: string;
+  email: string;
 }
 
 export const registerUser = async (data: RegisterData) => {
-    try {
-        const response = await axios.post('http://localhost:5000/register', data);
-        return response.data;
-    } catch (error) {
-        throw error;
+  try {
+    const response = await axios.post('http://localhost:5000/register', data);
+    if (response.data.token) {
+      localStorage.setItem('token', response.data.token);
     }
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 };
