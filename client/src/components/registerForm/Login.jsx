736b1974878bb6653afa372a13d194efa2c1206c
albamartinmz <albamartinsonido@gmail.com>
2024-04-09 11:08:03 +0200

    change-login-form

--- client/src/components/registerForm/Login.jsx
+++ client/src/components/registerForm/Login.jsx
@@ -3,13 +3,13 @@
 export const Login = () => {
   return (
     <div>
-      <h1>Sign In</h1>
+      <h1>Log In</h1>
       <form>
         <label htmlFor="email">Email</label>
         <input type="email" id="email" name="email" />
         <label htmlFor="password">Password</label>
         <input type="password" id="password" name="password" />
-        <button type="submit">Sign In</button>
+        <button type="submit">Log In</button>
       </form>
       
     </div>
