5ce629c052a9b35a87a695ec9d047ecb10e3ccd2
Leite <leite.way@gmail.com>
2024-04-09 13:00:16 +0200

    updated-token-in-register-form-added-secret-key-in-env-example-file

--- client/src/components/registerForm/RegisterForm.jsx
+++ client/src/components/registerForm/RegisterForm.jsx
@@ -5,19 +5,19 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import AppleIcon from '@mui/icons-material/Apple';
 import GoogleIcon from '@mui/icons-material/Google';
 import createUser from '../../services/register-services';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
 export const RegisterForm = () => {
-  const navigate = useNavigate();
   const { register, handleSubmit } = useForm();
 
   //formState: { errors } 
 
   const onSubmit = async (data) => {
     try {
-      const registrationResult = await createUser(data);
-      if (registrationResult.success) {
+      const response = await createUser(data);
+      if (response.success) {
         console.log('Usuario registrado exitosamente');
+        localStorage.setItem('token', response.data.token);
         // Redirige al usuario o realiza alguna otra acción después del registro exitoso
       } 
     } catch (error) {
