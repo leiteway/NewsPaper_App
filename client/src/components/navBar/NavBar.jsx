45eb8e83e36a363e83d958c60d3f41ddcde2706d
DesireeCSilva <desiresilva1987@gmail.com>
2024-04-16 21:36:53 +0200

    changes-on-several-components-favicon-added-and-some-more

--- client/src/components/navBar/NavBar.jsx
+++ client/src/components/navBar/NavBar.jsx
@@ -2,12 +2,13 @@ import './NavBar.css'
 import Box from '@mui/material/Box';
 import { useUserContext } from '../../context/UserContext';
 import LogOutButton from '../LogOutButton';
+import { Link } from 'react-router-dom'
 
 export default function NavBar() {
   const { isAuthenticated } = useUserContext()
   return (
     <div className="navbar">
-        <img className="logotipo" src='/src/assets/images/Logo/logo tech-3-AdminScreen.svg' alt="" />
+        <Link to="/" replace className='logo-nav'><img className="logotipo" src='/src/assets/images/Logo/logo tech-3-AdminScreen.svg' alt="" /></Link>
         <Box>
             <input className="searchBar" placeholder="Escribe tu búsqueda aquí..." />
         </Box>
