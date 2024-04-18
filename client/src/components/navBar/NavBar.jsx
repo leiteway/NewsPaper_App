c1b9f55cd78ab1f198a12874715e517a3dc234c7
DesireeCSilva <desiresilva1987@gmail.com>
2024-04-18 15:32:24 +0200

    last-chanages

--- client/src/components/navBar/NavBar.jsx
+++ client/src/components/navBar/NavBar.jsx
@@ -8,7 +8,7 @@ export default function NavBar() {
   const { isAuthenticated } = useUserContext()
   return (
     <div className="navbar">
-        <Link to="/" replace className='logo-nav'><img className="logotipo" src='/src/assets/images/Logo/logo tech-3-AdminScreen.svg' alt="" /></Link>
+        <Link to="/home" replace className='logo-nav'><img className="logotipo" src='/src/assets/images/Logo/logo tech-3-AdminScreen.svg' alt="" /></Link>
         <Box>
             <input className="searchBar" placeholder="Escribe tu búsqueda aquí..." />
         </Box>
