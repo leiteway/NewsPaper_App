9a5677febecc8a5c0b432a0fe51116d2cc64756c
Leite <leite.way@gmail.com>
2024-05-13 12:16:19 +0200

    chore(server):add-new-config-for-deploy-WIP

--- server/config.ts
+++ server/config.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 
 export const DB_DEV_NAME = <string> process.env.DB_DEV_NAME  || "";
+export const DB_HOST = <string> process.env.DB_HOST  || "";
 export const DB_USER= <string> process.env.DB_USER  || "";
 export const DB_PASSWORD= <string> process.env.DB_PASSWORD  || "";
 export const DB_PORT = <string> process.env.DB_PORT;
